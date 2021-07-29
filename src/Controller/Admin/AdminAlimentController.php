e59be457e201b45f118785d02f04dd726c1a4203
Rodolphe <dutchr@live.fr>
2021-07-29 09:36:47 +0200

    Adding EditPage w/ form and style

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aliment;
+use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +17,15 @@ class AdminAlimentController extends AbstractController
       $aliments = $repository->findAll();
         return $this->render('admin/admin_aliment/adminAliments.html.twig', [
             'aliments' => $aliments,
+        ]);
+    }
+    #[Route('/admin/aliment/{id}', name: 'admin_aliments_edit')]
+    public function edition(Aliment $aliment): Response
+    {
+      $form = $this->createForm(AlimentType::class, $aliment);
+        return $this->render('admin/admin_aliment/editAliment.html.twig', [
+            'aliment' => $aliment,
+            'form' => $form->createView()
         ]);
     }
 }
