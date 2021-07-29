f7bdd252d0c2422145949268366feaec5b8ed30d
Rodolphe <dutchr@live.fr>
2021-07-29 10:28:05 +0200

    refactor Edit by New&Edit on same method
    works fine

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Aliment;
 use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,13 +20,27 @@ class AdminAlimentController extends AbstractController
             'aliments' => $aliments,
         ]);
     }
+
+    
+    #[Route('/admin/aliment/new', name: 'admin_aliments_new')]
     #[Route('/admin/aliment/{id}', name: 'admin_aliments_edit')]
-    public function edition(Aliment $aliment): Response
+    public function addAndEdit(Aliment $aliment = null, Request $request): Response
     {
+      $manager = $this->getDoctrine()->getManager();
+      if (!$aliment) {
+        $aliment = new Aliment();
+      }
       $form = $this->createForm(AlimentType::class, $aliment);
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) {
+        $manager->persist($aliment);
+        $manager->flush();
+        return $this->redirectToRoute('admin_aliments');
+      }
         return $this->render('admin/admin_aliment/editAliment.html.twig', [
             'aliment' => $aliment,
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'edition'=> $aliment->getId() !== null
         ]);
     }
 }
